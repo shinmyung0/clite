e8e9e821205b58f61a3e2df141eb719e99687c58
Shin Myung Yoon <shiyoon@stubhub.com>
2016-08-10 23:49:14 -0700

    Finished coding up usage formatter

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Usage display example
 
-Main program usage text
+1. Program usage text
 
-```bash
+```
 > program
 usage: program <command>
 
 Available commands are:
     cmd        some random command
     cmd2       another random command
-    foocmd     another one, it's another one!
+    fooCmd     another one, oh it's another one!
 
 ```
 
-Usage text format per command
+2. Usage text format per command
 
-```bash
+```
 > program cmd
 usage: program cmd <requiredArg> [<optionalArg>] [<optionalArg2>]
 
@@ -25,6 +25,8 @@ Arguments are:
     optionalArg2     some argument that is also optional
 
 ```
+
+The format is heavily influenced by [Hashicorp's cli tool](https://github.com/mitchellh/cli)
 
 # Code example
 
@@ -38,22 +40,21 @@ import (
 )
 
 func main() {
-    format := goformat.Program("program")
-                      .hasCommand("cmd", "some random command")
-                      .hasCommand("cmd2", "another random command")
-                      .hasCommand("foocmd", "another one, it's another one!")
-    format.Command("cmd")
-          .hasArg("required", "some argument that is required to run the command", true)
-          .hasArg("optionalArg", "some argument that is optional", false)
-          .hasArg("optionalArg2", "some argument that is also optional", false)
+    prog := goformat.Program("program")
+    prog.HasCommand("cmd", "some random command")
+    prog.HasCommand("cmd2", "another random command")
+    prog.HasCommand("foocmd", "another one, it's another one!")
+
+    cmd := prog.Command("cmd")
+    cmd.HasArg("required", "some argument that is required to run the command", true)
+    cmd.HasArg("optionalArg", "some argument that is optional", false)
+    cmd.HasArg("optionalArg2", "some argument that is also optional", false)
 
     // Will print main usage text
     fmt.Print(format)
     // Will print usage text for cmd
     fmt.Print(format.ForCommand("cmd"))
 
-
 }
-
 
 ```
--- usage.go
+++ usage.go
@@ -1,15 +1,103 @@
 package goformat
 
-type UsageFormat struct {
-	Program  string
-	Commands map[string]Options
+import "fmt"
+
+type ProgramFormat struct {
+	Name       string
+	Commands   map[string]*CommandFormat
+	longestCmd int
 }
 
-type Options struct {
-	Args map[string]Arg
+type CommandFormat struct {
+	Program    string
+	Name       string
+	Summary    string
+	Arguments  map[string]*Arg
+	longestArg int
 }
 
 type Arg struct {
+	Name     string
 	Summary  string
-	Optional bool
+	required bool
+}
+
+const DELIM = "    "
+
+func Program(name string) *ProgramFormat {
+	return &ProgramFormat{name, make(map[string]*CommandFormat), 0}
+}
+
+func (p *ProgramFormat) Command(name string) *CommandFormat {
+	return p.Commands[name]
+}
+
+func (p *ProgramFormat) HasCommand(name, summary string) *ProgramFormat {
+	cmdLen := len(name)
+	cmd := &CommandFormat{p.Name, name, summary, make(map[string]*Arg), 0}
+	p.Commands[name] = cmd
+	if cmdLen > p.longestCmd {
+		p.longestCmd = cmdLen
+	}
+	return p
+}
+
+func (c *CommandFormat) HasArg(name, summary string, required bool) *CommandFormat {
+	argLen := len(name)
+	arg := &Arg{name, summary, required}
+	c.Arguments[name] = arg
+	if argLen > c.longestArg {
+		c.longestArg = argLen
+	}
+	return c
+}
+
+func genPadding(num int) string {
+	result := ""
+	for ; num > 0; num-- {
+		result += " "
+	}
+	return result
+}
+
+func (p *ProgramFormat) String() string {
+
+	result := fmt.Sprintf("usage: %s <command>\n\n", p.Name)
+	result += "Available commands are:\n"
+	for cmd, frmt := range p.Commands {
+		result += DELIM
+		result += cmd
+		result += genPadding(p.longestCmd - len(cmd))
+		result += DELIM
+		result += frmt.Summary
+		result += "\n"
+	}
+	return result
+}
+
+func (c *CommandFormat) String() string {
+
+	result := fmt.Sprintf("usage: %s %s", c.Program, c.Name)
+	for arg, frmt := range c.Arguments {
+		result += " "
+		if !frmt.required {
+			result += "[<" + arg + ">]"
+		} else {
+			result += "<" + arg + ">"
+		}
+	}
+	if len(c.Arguments) > 0 {
+		result += "\n\n"
+		result += "Arguments are:\n"
+		for arg, frmt := range c.Arguments {
+			result += DELIM
+			result += arg
+			result += genPadding(c.longestArg - len(arg))
+			result += DELIM
+			result += frmt.Summary
+			result += "\n"
+		}
+	}
+
+	return result
 }
